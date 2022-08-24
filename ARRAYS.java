4e763ecba4475d9b29b1e69343b1b9d41c436d76
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-08-24 08:10:40 +0530

    Update ARRAYS.java

--- ARRAYS.java
+++ ARRAYS.java
@@ -128,5 +128,109 @@ public class ARRAYS {
        return sum;
     }
      */
+ 
+   
+    /*
+    Problem 867
+     public static int[][] transpose(int[][] matrix) {
+        int[][] ans=new int[matrix[0].length][matrix.length];
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[i].length;j++)
+            {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
+    }
+     */
+
+    /*
+    Problem 1
+     public static int[] twoSum(int[] nums, int target) {
+        for(int i=1; i<nums.length; i++) {
+            for(int j=i; j<nums.length; j++) {
+                if (nums[j-i]+nums[j] == target) {
+                    return new int[]{j-i,j};
+                }
+            }
+        }
+        return null;
+        }
+     */
+
+    /*
+    Problem 1304
+    public static int[] sumZero(int n) {
+        if(n == 1)
+            return new int[]{0};
+
+        int[] result = new int[n];
+
+        for(int i = 0; i < n-1; i+=2) {
+            result[i] = i + 1;
+            result[i+1] = (i + 1) * -1;
+        }
+        return result;
+    }
+     */
+
+    /*
+    Problem 66
+  public static int[] plusOne(int[] digits) {
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i] ==9){
+                digits[i]=0;
+            }else{
+                digits[i]++;
+                return digits;
+            }
+        }
+        int[] res = new int[digits.length+1];
+        res[0]=1;
+        return res;
+    }
+     */
+
+    /*
+    Problem 54
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        int tne=matrix.length*matrix[0].length;
+        int rowmin=0, colmin=0, colmax=matrix[0].length-1, rowmax=matrix.length-1;
+        int count=0;
+        List<Integer> ans=new ArrayList<>();
+        while(count<tne){
+            //top wall
+            for(int i=colmin; i<=colmax && count<tne; i++){
+                ans.add(matrix[rowmin][i]);
+                count++;
+            }
+            rowmin++;
+
+            //right wall
+            for(int i=rowmin; i<=rowmax && count<tne; i++){
+                ans.add(matrix[i][colmax]);
+                count++;
+            }
+            colmax--;
+
+            //bottom wall
+            for(int i=colmax; i>=colmin && count<tne; i--){
+                ans.add(matrix[rowmax][i]);
+                count++;
+            }
+            rowmax--;
+
+            //left wall
+            for(int i=rowmax; i>=rowmin && count<tne;i--){
+                ans.add(matrix[i][colmin]);
+                count++;
+            }
+            colmin++;
+        }
+        return ans;
+    }
+     */
+
 
 }
