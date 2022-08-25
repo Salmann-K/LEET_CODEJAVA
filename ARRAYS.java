eda912b8c5466a1ee13ecd5d16398c5bb57c7e92
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-08-25 08:11:59 +0530

    Update ARRAYS.java

--- ARRAYS.java
+++ ARRAYS.java
@@ -317,5 +317,45 @@ public class ARRAYS {
         return max;
     }
      */
+ 
+       /*
+    Problem 1886
+    public static boolean findRotation(int[][] mat, int[][] target) {
+        int n= mat.length;
+    //Checking Matrices Are equal Are not
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <n ; j++) {
+                if(equals(mat, target))
+                    return true;
+                else
+                    rotate(mat);
+            }
+        }
+        return false;
+    }
+    public static boolean equals(int[][] mat, int[][] target) {
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0; j < mat.length; j++) {
+                if (mat[i][j] != target[i][j])
+                    return false;
+            }
+        }
+        return true;
+    }
+    public static void rotate(int [][] mat)
+    {
+        int n= mat.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[n - 1 - j][i];
+                mat[n - 1 - j][i] = mat[n - 1 - i][n - 1 - j];
+                mat[n - 1 - i][n - 1 - j] = mat[j][n - 1 - i];
+                mat[j][n - 1 - i] = temp;
+            }
+        }
+    }
+     */
+
 
 }
