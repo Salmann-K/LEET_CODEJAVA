8f046285521c4bf5e8443224c99553f80fd818e9
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-08-26 08:19:18 +0530

    Update ARRAYS.java

--- ARRAYS.java
+++ ARRAYS.java
@@ -396,6 +396,36 @@ public class ARRAYS {
     }
     */
 
+     
+    /*
+    //Problem 1534
+    public static int countGoodTriplets(int[] arr, int a, int b, int c) {
+            int i,j,k,count=0;
+            for(i=0;i<(arr.length)-2;i++)
+                for(j=i+1;j<(arr.length)-1;j++)
+                    if(Math.abs(arr[i]-arr[j]) <=a)
+                        for(k=j+1;k<arr.length;k++){
+                            if(Math.abs(arr[j]-arr[k]) <=b)
+                                if(Math.abs(arr[i]-arr[k]) <=c)
+                                    count++;
+                        }
+            return count;
+        }
+*/
+
+    /*
+    Problem 1470
+  public static int[] shuffle(int[] nums, int n) {
+        int[] res = new int[2 * n];
+        int k = 0;
+        for (int i = 0; i < n; i = i++) {
+            res[i] = nums[k];
+            res[i + 1] = nums[k + n];
+            k++;
+        }
+        return res;
+    }
+     */
 
 
 }
