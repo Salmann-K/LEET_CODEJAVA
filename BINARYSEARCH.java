28b488d0fb8880759035a2dabb994e1c68a21128
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-09-01 08:17:19 +0530

    Update BINARYSEARCH.java

--- BINARYSEARCH.java
+++ BINARYSEARCH.java
@@ -154,6 +154,20 @@ public class BINARYSEARCH {
      */
 
 
-  
+        /*
+    Problem 704
+      public static int search(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1; }
+     */
+
   
 }
