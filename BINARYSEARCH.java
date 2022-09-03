dd17feeefc308e17e5be04ebdf1942ce38a0e72f
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-09-03 08:11:54 +0530

    Update BINARYSEARCH.java

--- BINARYSEARCH.java
+++ BINARYSEARCH.java
@@ -304,6 +304,90 @@ public class BINARYSEARCH {
         return median;
     }
      */
+  
+  
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////// BINARY SEARCH IN 2D MATRIX ///////////////////////////////////////////////////////////
+  
+   static int [] Binarysearch(int[][] matrix, int row, int cStart, int cEnd, int target)
+    {
+        while(cStart<=cEnd)
+        {
+            int mid= cStart + (cEnd-cStart)/2;
+            if(matrix[row][mid]==target)
+            {
+                return new int[] {row,mid};
+            }
+            if(matrix[row][mid]<target)
+            {
+                cStart=mid+1;
+            }
+            if(matrix[row][mid]>target)
+            {
+                cEnd=mid-1;
+            }
+        }
+        return new int []{-1,-1};
+    }
+
+    static int [] search(int[][]matrix, int target)
+    {
+        int rows= matrix.length;
+        int col= matrix[0].length;
+        if(rows==1)
+        {
+            return Binarysearch(matrix,0,0,col-1,target);
+        }
+        int rStart=0;
+        int rEnd=rows-1;
+        int cMid=col/2;
+
+        while(rStart<(rEnd-1))
+        {
+            int mid= rStart+(rEnd-rStart)/2;
+            if(matrix[mid][cMid]== target)
+            {
+                return new int[]{mid,cMid};
+            }
+            if(matrix[mid][cMid]<target)
+            {
+                rStart=mid;
+            }else
+            {
+                rStart=mid;
+            }
+
+        }
+        // Checking in the remaining two rows
+
+        if(matrix[rStart][cMid]==target)
+        {
+            return new int[]{rStart,cMid};
+        }
+        if(matrix[rStart+1][cMid]==target)
+        {
+            return new int[]{rStart+1,cMid};
+        }
+
+        // last four part Checking
+
+        if(target<=matrix[rStart][cMid-1])
+        {
+            return Binarysearch(matrix,rStart,0,cMid-1,target);
+        }
+        if(target>=matrix[rStart][cMid+1] && target<=matrix[rStart][col-1])
+        {
+            return Binarysearch(matrix,rStart,cMid+1,col-1,target);
+        }
+        if(target<=matrix[rStart+1][cMid-1])
+        {
+            return Binarysearch(matrix,rStart+1,0,cMid-1,target);
+        }else
+        {
+            return Binarysearch(matrix,rStart+1,cMid+1,col-1,target);
+        }
+
+    }
 
   
 }
