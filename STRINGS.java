f56ea0dc02c8613c1fc6ce215deeed9487b50e0c
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-09-06 14:37:39 +0530

    Add files via upload

---
+++ STRINGS.java
@@ -0,0 +1,42 @@
+package com.company.UPLOAD;
+
+public class STRINGS {
+
+     /*
+    problem 2011;
+    public static int finalValueAfterOperations(String[] operations)
+    {
+        int x=0;
+        for (int i = 0; i <operations.length ; i++) {
+            if(operations[i].equals("X++") || operations[i].equals("++X"))
+                x++;
+            else
+                x--;
+        }
+        return x;
+    }
+     */
+    /*
+    problem- 2114
+    public static int mostWordsFound(String [] sentences) {
+        int max=0;
+        for (int j = 0; j < sentences.length; j++) {
+            max=Math.max(sentences[j].split(" ").length,max);
+        }
+
+        return max;
+    }
+     */
+
+    /*
+        problem 1528;
+        public static String restoreString(String s, int[] indices) {
+        char[] t = new char[s.length()];
+        for(int i = 0;i<t.length;i++){
+            t[indices[i]] = s.charAt(i);
+        }
+        return String.valueOf(t);
+    }
+     */
+
+}
