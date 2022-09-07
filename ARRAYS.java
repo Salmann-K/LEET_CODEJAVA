5555ba1a5ca7a9f207034e317637e588ca7b45a9
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-09-07 19:52:57 +0530

    Update ARRAYS.java

--- ARRAYS.java
+++ ARRAYS.java
@@ -658,5 +658,23 @@ public class ARRAYS {
     }
      */
 
+      public int removeDuplicates(int[] nums) {
+  
+    if (nums.length== 0 || nums.length == 1)
+        return nums.length;
+  
+    // To store index of next unique element
+    int j = 0;
+  
+
+    //  maintain another updated index i.e. j
+    for (int i = 0; i < nums.length-1; i++)
+        if (nums[i] != nums[i+1])
+            nums[j++] = nums[i];
+  
+    nums[j++] = nums[nums.length-1];
+  
+    return j;
+}
 
 }
