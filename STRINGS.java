d8aeffc50b3b7e62d80bb8d3d96c68b366b184e6
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-09-19 07:24:21 +0530

    Update STRINGS.java

--- STRINGS.java
+++ STRINGS.java
@@ -628,5 +628,22 @@ public static String longestCommonPrefix(String[] strs) {
         return sb.toString();}
     */
 
+     
+     
+    /*
+    387. First Unique Character in a String
+    public int firstUniqChar(String s) {
+        int freq[] = new int[26];
+        for(int i = 0; i < s.length(); i ++)
+            freq[s.charAt(i) - 'a'] ++;
+
+        for(int i = 0; i < s.length(); i ++)
+            if(freq[s.charAt(i) - 'a'] == 1)
+                return i;
+
+        return - 1;
+    }
+     */
+
 
 }
