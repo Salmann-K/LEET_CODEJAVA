2351a4a9be2ca33b81fcc8b0910ea294a16838e6
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-09-20 07:42:42 +0530

    Add files via upload

---
+++ PATTERN.java
@@ -0,0 +1,16 @@
+package com.company.UPLOAD;
+
+public class PATTERN {
+
+     /*
+    static void pattern3(int n)
+    {
+        for (int row = 0; row <n ; row++) {
+            for (int col = 0; col <n-row ; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+*/
+}
