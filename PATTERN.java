bef8229a4e0ee569c89120988b0346fd382f4ee0
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-09-24 07:09:34 +0530

    Update PATTERN.java

--- PATTERN.java
+++ PATTERN.java
@@ -236,6 +236,46 @@ public class PATTERN {
     }
      */
   
-     
+       /*
+    static void pattern15(int n) {
+        for (int i=1; i<= n ; i++)
+        {
+            for (int j = n; j > i ; j--)
+            {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for (int k = 1; k < 2*(i -1) ;k++)
+            {
+                System.out.print(" ");
+            }
+            if( i==1)
+            {
+                System.out.println("");
+            }
+            else
+            {
+                System.out.println("*");
+            }
+        }
+        for (int i=n-1; i>= 1 ; i--)
+        {
+            for (int j = n; j > i ; j--)
+            {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for (int k = 1; k < 2*(i -1) ;k++)
+            {
+                System.out.print(" ");
+            }
+            if(i==1)
+                System.out.println("");
+            else
+                System.out.println("*");
+        }
+    }
+    */
+  
      
 }
