2c1e16b20b1df8c70331f0bb1e0ef240ad45bb2d
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-09-25 07:01:36 +0530

    Add files via upload

---
+++ MATHS.java
@@ -0,0 +1,20 @@
+package com.company.UPLOAD;
+
+public class MATHS {
+
+    /*
+    // Problem 202
+    public static boolean isHappy(int n) {
+        if(n==1) return true;
+        else if(n>1 && n<7) return false;
+        else{
+            int sum=0;
+            while(n!=0){
+                sum+=(n%10)*(n%10);
+                n/=10;
+            }
+            return isHappy(sum);
+        }}
+*/
+
+}
