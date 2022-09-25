5925638d18510061ac3d55d87221e26b71bb4774
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-09-25 07:02:27 +0530

    Update MATHS.java

--- MATHS.java
+++ MATHS.java
@@ -30,5 +30,15 @@ public class MATHS {
     }
      */
 
+        /*
+    // Problem 342
+        public static boolean isPowerOfFour(int n) {
+            if(n<=0){return false;}
+            if(n==1){return true;}
+            if(n%4==0){return isPowerOfFour(n/4);}
+            return false;
+        }
+     */
+
 
 }
