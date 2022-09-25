5e8315b11c554d4651ff433aee42503d73063dca
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-09-25 07:03:11 +0530

    Update MATHS.java

--- MATHS.java
+++ MATHS.java
@@ -39,6 +39,16 @@ public class MATHS {
             return false;
         }
      */
+    
+    
+        /*
+    //Factorial
+    static long factorial(int N){
+    if(N==1 || N==0){return 1;}
+    return N*factorial(N-1);
+    }
+     */
+    
 
 
 }
