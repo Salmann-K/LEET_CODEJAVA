3a5d36f4faf66d09695e986b6bb715017958f879
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-09-28 07:09:20 +0530

    Update ARRAYS.java

--- ARRAYS.java
+++ ARRAYS.java
@@ -908,6 +908,26 @@ public class ARRAYS {
         return c;
     }
      */
- 
+   
+    /*
+    74. Search a 2D Matrix
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int start = 0;
+        int end = (n*m)-1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(matrix[mid/m][mid%m]==target){
+                return true;
+            }else if(matrix[mid/m][mid%m]<target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return false;
+    }
+    */
   
 }
