7567863d5a53c6c3ecbda1e56d8c309a3048ba60
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-09-29 07:25:00 +0530

    Update ARRAYS.java

--- ARRAYS.java
+++ ARRAYS.java
@@ -967,6 +967,26 @@ public class ARRAYS {
         return count;
     }
         */
- 
+        
+      /*
+      
+      public List<Integer> findClosestElements(int[] arr, int k, int x) {
+
+        int left = 0, right = arr.length - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - arr[mid] > arr[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+
+        List<Integer> result = new ArrayList<>(k);
+        for (int i = left; i < left + k; i++) {
+            result.add(arr[i]);
+        }
+        return result;
+    }
+      */
   
 }
