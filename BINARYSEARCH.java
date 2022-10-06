2a4e670ecd6fcc1b44ebe1bb42d80b642471807f
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-10-06 07:07:36 +0530

    Update BINARYSEARCH.java

--- BINARYSEARCH.java
+++ BINARYSEARCH.java
@@ -64,6 +64,7 @@ public class BINARYSEARCH {
   
     /*
     Problem 367
+
        public static boolean isPerfectSquare(int num) {
         if(num == 0 || num ==1) return true;
         int n = 1;
