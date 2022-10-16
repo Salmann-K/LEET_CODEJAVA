963c11055185fcb44dc368698e84bbcc01ace42d
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-10-16 17:22:42 +0530

    Update MATHS.java

--- MATHS.java
+++ MATHS.java
@@ -166,6 +166,24 @@ public class MATHS {
         return sum-pr;
     }
        */
+    
+     /*
+    //problem 2169
+    public static int countOperations(int num1, int num2) {
+    int op=0;
+    while(num1>0 && num2>0){
+
+        if(num1>=num2){
+            num1=num1-num2;
+            op++;
+        }else  {
+            num2=num2-num1;
+            op++;
+        }
+    }
+    return op;
+    }
+    */
 
     
 }
