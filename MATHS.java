d8c790835c490441ed56ce842b03fac9d1fdf089
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-10-16 17:23:17 +0530

    Update MATHS.java

--- MATHS.java
+++ MATHS.java
@@ -186,4 +186,30 @@ public class MATHS {
     */
 
     
+    /*
+    202. Happy Number
+    public static boolean isHappy(int n) {
+    int slow=n;
+    int fast = n;
+
+    do{
+        slow=findsquare(slow);
+        fast=findsquare(findsquare(fast));
+    }while(fast!=slow);
+    if(slow==1){
+        return true;
+    }
+    return false;
+    }
+    private static int findsquare(int num){
+        int ans=0;
+        while(num>0){
+            int rem=num%10;
+            ans+=rem*rem;
+            num=num/10;
+        }
+        return ans;
+    }
+     */
+    
 }
