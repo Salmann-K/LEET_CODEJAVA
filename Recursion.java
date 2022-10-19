fcdf1a76ce2f7548cba2fd1bf3f70f2607689fe5
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-10-19 07:09:09 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,33 @@
+package com.company.UPLOAD;
+
+public class Recursion {
+
+     /*
+    static int fibo(int n)
+    {
+        if(n<2)
+            return n;
+        return fibo(n-1)+fibo(n-2);
+    }
+     */
+    /*
+    static int bsearch(int [] arr , int target , int s , int e)
+    {
+        if(s>e)
+            return -1;
+        int m=s+(e-s)/2;
+        if(arr[m]==target)
+        {
+            return m;
+        }
+        if(arr[m]>target)
+        {
+            return bsearch(arr,target,s,m-1);
+        }
+        else
+            return bsearch(arr,target,m+1,m);
+    }
+     */
+
+    
+}
