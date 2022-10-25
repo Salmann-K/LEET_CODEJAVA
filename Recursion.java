f7254c3bd5edf4a159fd41efb479c0faff31a66e
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-10-25 06:35:33 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -133,6 +133,15 @@ public class Recursion {
     }
      */
     
-   
+     /*
+    static int length(String str)
+    {
+        if (str.equals(""))
+            return 0;
+        else
+            return length(str.substring(1)) + 1;
+
+    }
+     */
     
 }
