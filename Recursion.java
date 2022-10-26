4cbcea18da5ad5f8ddebabd258de62531e3d3d4f
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-10-26 07:24:04 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -3,6 +3,25 @@ package com.company.UPLOAD;
 public class Recursion {
      
       /*
+    //Problem 326
+    public static boolean isPowerOfThree(int n) {
+        if(n<=0)
+        {
+            return false;
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        if(n%3==0)
+        {
+            return isPowerOfThree(n/3);
+        }
+        return false;
+    }
+     */
+     
+      /*
     //Problem 231
     public static boolean isPowerOfTwo(int n) {
     if(n<=0)
