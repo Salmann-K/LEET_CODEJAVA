ec3f1f0a6b7bd011b516396637e617c9789b0731
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-10-27 07:17:49 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -275,5 +275,15 @@ public class Recursion {
         return n+recursum(n-1);
     }
      */
+     
+      /*
+    public static int factorial(int n) {
+    if(n==1 || n==0)
+    {
+        return 1;
+    }
+    return n*factorial(n-1);
+    }
+     */
     
 }
