088802057d10f3fd56b341fe4b2cf3de8289adc2
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-10-28 07:15:22 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -320,5 +320,18 @@ public class Recursion {
         return isSorted(arr, n - 1);
     }
    */
+     
+     /*
+    public static int sum=0;
+    static void reverse(int n)
+    {
+        if(n==0) {
+            return;
+        }
+        int rem=n%10;
+        sum=sum*10+rem;
+        reverse(n/10);
+    }
+    */
     
 }
