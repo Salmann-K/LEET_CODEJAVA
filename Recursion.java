c3f2809709bfa984cdffbb5d96ba5d0fe5a9ee07
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-10-30 07:08:52 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -206,6 +206,44 @@ public class Recursion {
       }
     }
 */
+     
+      /*
+    // * ** *** **** pattern using Recursion 2
+    public static void pattern(int r,int c)
+    {
+        if(r==0){
+            return;
+        }
+        if(r>c){
+            pattern(r,c+1);
+            System.out.print("*");
+        }
+        else{
+            pattern(r-1,0);
+            System.out.println();
+        }
+    }
+     */
+
+    /*
+    public static void bubblesort(int [] arr , int r,int c)
+    {
+        if(r==0){
+            return;
+        }
+        if(r>c){
+            if(arr[c]>arr[c+1]){
+                int temp=arr[c];
+                arr[c]=arr[c+1];
+                arr[c+1]=temp;
+            }
+            bubblesort(arr,r,c+1);
+        }
+        else{
+            bubblesort(arr,r-1,0);
+        }
+    }
+*/
 
 
      /*
