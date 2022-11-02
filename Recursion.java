41e81c87846f407a58e94e3aae21b6373910de50
Salmann-K <103230723+Salmann-K@users.noreply.github.com>
2022-11-02 07:07:43 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -597,7 +597,46 @@ public class Recursion {
     }
      */
 
-   
+    /*
+    // Subset Of Array
+    public static List<List<Integer>> subset(int [] arr){
+        List<List<Integer> > outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for (int num : arr){
+            int n= outer.size();
+            for (int i = 0; i <n ; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }   }
+        return outer;
+    }
+     */
+
+    /*
+        // Subset Of Array having duplicate
+        public static List<List<Integer>> subsetduplicate(int [] arr){
+            List<List<Integer> > outer = new ArrayList<>();
+            outer.add(new ArrayList<>());
+            int start=0;
+            int end=0;
+            for (int i=0; i<arr.length; i++){
+                start=0;
+                if(i>0 && arr[i]==arr[i-1]) {
+                    start=end+1;
+                }
+                end=outer.size()-1;
+                int n= outer.size();
+                for (int j = start; j <n ;j++) {
+                    List<Integer> internal = new ArrayList<>(outer.get(j));
+                    internal.add(arr[i]);
+                    outer.add(internal);
+                }   }
+            return outer;
+        }
+    */
+     
+     
       /*
     // * ** *** **** pattern using Recursion 2
     public static void pattern(int r,int c)
